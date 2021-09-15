dab84fcd4265242dd48eab62eec20e8090615cd3
Raphael Coelho <raphaelcoel@gmail.com>
2021-09-15 15:42:25 -0300

    cria projeto e primeira lambda utilizando o serverless

---
+++ handler.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports.listarPacientes = async (event) => {
+
+  console.log(event)
+  const pacientes = [
+    { id: 1, nome: "Maria", dataNascimento: '1984-01-11' },
+    { id: 2, nome: "Joao", dataNascimento: '1983-01-11 ' },
+    { id: 3, nome: "Jose", dataNascimento: '1959-07-15' }
+  ];
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        pacientes
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
