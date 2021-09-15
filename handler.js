41b6599a6df7c65e213c5f47b008ec17d877caec
Raphael Coelho <raphaelcoel@gmail.com>
2021-09-15 16:27:28 -0300

    Cria endpoint /paciente/{id}

--- handler.js
+++ handler.js
@@ -19,7 +19,29 @@ module.exports.listarPacientes = async (event) => {
       2
     ),
   };
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.obterPaciente = async (event) => {
+
+  console.log(event)
+  const pacientes = [
+    { id: 1, nome: "Maria", dataNascimento: '1984-01-11' },
+    { id: 2, nome: "Joao", dataNascimento: '1983-01-11 ' },
+    { id: 3, nome: "Jose", dataNascimento: '1959-07-15' }
+  ];
+
+  const { pacienteId } = event.pathParameters;
+
+  const paciente = pacientes.find(paciente => paciente.id == pacienteId)
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        paciente
+      },
+      null,
+      2
+    ),
+  };
 };
